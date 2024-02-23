43eaf7ed1666a9d643082514c2a8979deb6b6ede
AbelHristodor <habelor@gmail.com>
2024-02-23 16:32:13 +0100

    feat: golang

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -57,6 +57,29 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 end
 
+local golang_on_attach = function(client, bufnr)
+  -- Autoapply code actions
+  vim.api.nvim_create_autocmd('BufWritePre', {
+    pattern = '*.go',
+    callback = function()
+      local params = vim.lsp.util.make_range_params()
+      params.context = { only = { 'source.organizeImports' } }
+
+      local result = vim.lsp.buf_request_sync(0, 'textDocument/codeAction', params)
+      for cid, res in pairs(result or {}) do
+        for _, r in pairs(res.result or {}) do
+          if r.edit then
+            local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+            vim.lsp.util.apply_workspace_edit(r.edit, enc)
+          end
+        end
+      end
+    end
+  })
+
+  return on_attach(client, bufnr)
+end
+
 return {
   -- LSP Configuration & Plugins
   'neovim/nvim-lspconfig',
@@ -131,17 +154,6 @@ return {
         filetypes = { 'Dockerfile', 'docker-compose' }
       },
       terraformls = {},
-      gopls = {
-        settings = {
-          gopls = {
-            completeUnimported = true,
-            usePlaceholders = true,
-            analyses = {
-              unusedparams = true,
-            },
-          }
-        }
-      },
       jsonls = {},
       bashls = {},
     }
@@ -177,6 +189,23 @@ return {
           end
         end)
       end
+    })
+
+
+    lspconfig.gopls.setup({
+      on_attach = golang_on_attach,
+      capabilities = capabilities,
+      opts = {
+        settings = {
+          gopls = {
+            completeUnimported = true,
+            usePlaceholders = true,
+            analyses = {
+              unusedparams = true,
+            },
+          }
+        }
+      }
     })
   end,
 }
